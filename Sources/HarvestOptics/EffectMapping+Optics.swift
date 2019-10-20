099a8023a5f91c5729d3a6012e582ef07d9b0163
Yasuhiro Inami <inamiy@gmail.com>
2019-10-20 14:55:46 +0900

    Change `lift` to `EffectMapping.transform`

--- Sources/HarvestOptics/EffectMapping+Optics.swift
+++ Sources/HarvestOptics/EffectMapping+Optics.swift
@@ -1,36 +1,31 @@
 import FunOptics
 import Harvest
 
-/// Lifts `EffectMapping` from `PartInput` to `WholeInput`.
-public func lift<WholeInput, PartInput, State, Queue, EffectID>(
-    input inputTraversal: AffineTraversal<WholeInput, PartInput>
-)
-    -> (_ mapping: Harvester<PartInput, State>.EffectMapping<Queue, EffectID>)
-    -> Harvester<WholeInput, State>.EffectMapping<Queue, EffectID>
+extension Harvester.EffectMapping
 {
-    return { mapping in
+    /// Transforms `EffectMapping` from `Input` to `WholeInput`.
+    public func transform<WholeInput>(
+        input inputTraversal: AffineTraversal<WholeInput, Input>
+    ) -> Harvester<WholeInput, State>.EffectMapping<Queue, EffectID>
+    {
         return .init { wholeInput, state in
             guard let partInput = inputTraversal.tryGet(wholeInput),
-                let (newState, effect) = mapping.run(partInput, state) else
+                let (newState, effect) = self.run(partInput, state) else
             {
                 return nil
             }
             return (newState, effect.mapInput { inputTraversal.set(wholeInput, $0) })
         }
     }
-}
 
-/// Lifts `EffectMapping` from `PartState` to `WholeState`.
-public func lift<WholeState, PartState, Input, Queue, EffectID>(
-    state stateTraversal: AffineTraversal<WholeState, PartState>
-)
-    -> (_ mapping: Harvester<Input, PartState>.EffectMapping<Queue, EffectID>)
-    -> Harvester<Input, WholeState>.EffectMapping<Queue, EffectID>
-{
-    return { mapping in
+    /// Transforms `EffectMapping` from `State` to `WholeState`.
+    public func transform<WholeState>(
+        state stateTraversal: AffineTraversal<WholeState, State>
+    ) -> Harvester<Input, WholeState>.EffectMapping<Queue, EffectID>
+    {
         return .init { input, wholeState in
             guard let partState = stateTraversal.tryGet(wholeState),
-                let (newPartState, effect) = mapping.run(input, partState) else
+                let (newPartState, effect) = self.run(input, partState) else
             {
                 return nil
             }
@@ -40,17 +35,5 @@ public func lift<WholeState, PartState, Input, Queue, EffectID>(
             return (newWholeState, effect)
         }
     }
-}
 
-/// Lifts `EffectMapping` from `<PartInput, PartState>` to `<WholeInput, WholeState>`.
-public func lift<WholeInput, PartInput, WholeState, PartState, Queue, EffectID>(
-    input inputTraversal: AffineTraversal<WholeInput, PartInput>,
-    state stateTraversal: AffineTraversal<WholeState, PartState>
-)
-    -> (_ mapping: Harvester<PartInput, PartState>.EffectMapping<Queue, EffectID>)
-    -> Harvester<WholeInput, WholeState>.EffectMapping<Queue, EffectID>
-{
-    return { mapping in
-        lift(state: stateTraversal)(lift(input: inputTraversal)(mapping))
-    }
 }
